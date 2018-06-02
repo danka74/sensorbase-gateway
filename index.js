811d77bf1a9a5dbee009128af68f1b514d9ce244
Daniel Karlsson <daniel.e.karlsson@gmail.com>
2018-06-02 13:23:46 +0200

    added login to firebase via web

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
-var express = require("express");
+const express = require("express");
+const bodyParser = require("body-parser")
 
 const app = express();
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
-var firebase = require("firebase");
-
-var config = require("./config");
-
+const firebase = require("firebase");
+const config = require("./config");
 firebase.initializeApp(config);
+var useerId = null;
 
-var SerialPort = require("serialport");
-
+const SerialPort = require("serialport");
 const serialportDevice = "/dev/ttyAMA0";
-const baudRate = 115200;
-
+const baudRate = 19200;
 var port = new SerialPort(serialportDevice, {
   baudRate: baudRate
 });
 
 // Read data that is available but keep the stream from entering "flowing mode"
 port.on("readable", () => {
-  console.log("Data:", port.read());
+  console.log("Data: ", port.read());
 });
+
+app.post('/auth', (req, res) => {
+  var  accessObject = req.body;
+  console.log(accessObject.accessToken);
+  console.log(accessObject.idToken);
+  var credential = firebase.auth.GoogleAuthProvider.credential(accessObject.idToken, accessObject.accessToken);
+  console.log(JSON.stringify(credential));
+  firebase.auth().signInAndRetrieveDataWithCredential(credential).then((user) => {
+    console.log("Logged in to Firebase: " + JSON.stringify(user));
+    userId = user.uid;
+  }).catch(
+    function(error) {
+        console.log("Error signing in to Firebase with user " +
+            error.email + ": " + error.message + " (" +
+            error.code + ")");
+        userId = null;
+    });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end();
+});
+
+app.get('/signout', (req, res) => {
+  firebase.auth().signOut().then(() => {
+    useerId = null;
+    console.log("Signed out");
+  })
+});
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
