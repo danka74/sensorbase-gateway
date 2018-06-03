0a6fe00deadd63194673f0f9bc54fd06db4abaa9
Daniel Karlsson <daniel.e.karlsson@gmail.com>
2018-06-03 15:28:37 +0200

    database fixexs

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const credentials = {key: privateKey, cert: certificate};
 const firebase = require("firebase");
 const config = require("./config");
 firebase.initializeApp(config);
-var useerId = null;
+const db = firebase.firestore();
+var userId = null;
 
 const SerialPort = require("serialport");
 const Readline = require('@serialport/parser-readline')
@@ -29,21 +30,28 @@ const parser = port.pipe(new Readline({ delimiter: '\n' }));
 // Read data that is available but keep the stream from entering "flowing mode"
 parser.on("data", (data) => {
   console.log("Data: ", data);
-  if(useerId != null) {
+  if(userId != null) {
     var sensorData = JSON.parse(data);
 
     var battery = sensorData.data[0];
     var temp = sensorData.data[1];
-    var hum = sensoorData.data[2];
+    var hum = sensorData.data[2];
     var pres = sensorData.data[3];
     var soil = sensorData.data[4];
 
-    var ref = firebase.database.collection("data/" + userId + "/data");
-
-    ref.set({quantity: "tempC", value: temp, time: firebase.Timestamp.now()});
-    ref.set({quantity: "humPerc", value: hum, time: firebase.Timestamp.now()});
-    ref.set({quantity: "presHPa", value: pres, time: firebase.Timestamp.now()});
-    ref.set({quantity: "moistPerc", value: soil, time: firebase.Timestamp.now()});
+    console.log("Saving to user: " + userId);
+    var ref = db.collection("data").doc(userId).collection("data");
+    var doc = {sensor: sensorData.sender, quantity: "voltV", value: battery, time: new Date()};
+    console.log(doc);
+    ref.add(doc).then((result) => {
+      console.log(result);
+    }).catch((error) => {
+      console.log(error);
+    });
+    // ref.add({sensor: sensorData.sender, quantity: "tempC", value: temp, time: firebase.Timestamp.now()});
+    // ref.add({sensor: sensorData.sender, quantity: "humPerc", value: hum, time: firebase.Timestamp.now()});
+    // ref.add({sensor: sensorData.sender, quantity: "presHPa", value: pres, time: firebase.Timestamp.now()});
+    // ref.add({sensor: sensorData.sender, quantity: "moistPerc", value: soil, time: firebase.Timestamp.now()});
 
   } else {
     console.log("Data received but not signed into Firebase");
@@ -51,14 +59,10 @@ parser.on("data", (data) => {
 });
 
 app.post('/auth', (req, res) => {
-  var  accessObject = req.body;
-  console.log(accessObject.accessToken);
-  console.log(accessObject.idToken);
-  var credential = firebase.auth.GoogleAuthProvider.credential(accessObject.idToken, accessObject.accessToken);
-  console.log(JSON.stringify(credential));
+  var credential = firebase.auth.GoogleAuthProvider.credential(req.body);
   firebase.auth().signInAndRetrieveDataWithCredential(credential).then((user) => {
     console.log("Logged in to Firebase: " + JSON.stringify(user));
-    userId = user.uid;
+    userId = user.user.uid;
   }).catch(
     function(error) {
         console.log("Error signing in to Firebase with user " +
@@ -73,7 +77,7 @@ app.post('/auth', (req, res) => {
 
 app.get('/signout', (req, res) => {
   firebase.auth().signOut().then(() => {
-    useerId = null;
+    userId = null;
     console.log("Signed out");
   })
 });
