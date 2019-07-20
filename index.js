e607593fc7deb0a6893814a324bedac93776aefb
danka74 <daniel.e.karlsson@gmail.com>
2019-07-20 08:49:33 +0000

    some changes...

--- index.js
+++ index.js
@@ -33,30 +33,58 @@ parser.on("data", (data) => {
   if(userId != null) {
     var sensorData = JSON.parse(data);
 
-    var battery = sensorData.data[0];
-    var temp = sensorData.data[1];
-    var hum = sensorData.data[2];
-    var pres = sensorData.data[3];
-    var soil = sensorData.data[4];
+    if(sensorData.data != "START") {
+      const battery = sensorData.data[0];
+      const temp = sensorData.data[1];
+      const hum = sensorData.data[2];
+      const pres = sensorData.data[3];
+      const soil = sensorData.data[4];
 
-    console.log("Saving to user: " + userId);
-    var ref = db.collection("data").doc(userId).collection("data");
-    var doc = {sensor: sensorData.sender, quantity: "voltV", value: battery, time: new Date()};
-    console.log(doc);
-    ref.add(doc).then((result) => {
-      console.log(result);
-    }).catch((error) => {
-      console.log(error);
-    });
-    // ref.add({sensor: sensorData.sender, quantity: "tempC", value: temp, time: firebase.Timestamp.now()});
-    // ref.add({sensor: sensorData.sender, quantity: "humPerc", value: hum, time: firebase.Timestamp.now()});
-    // ref.add({sensor: sensorData.sender, quantity: "presHPa", value: pres, time: firebase.Timestamp.now()});
-    // ref.add({sensor: sensorData.sender, quantity: "moistPerc", value: soil, time: firebase.Timestamp.now()});
-
+      console.log("Saving to user: " + userId);
+      store(sensorData.sender, "voltV", battery);
+      store(sensorData.sender, "tempC", temp);
+      store(sensorData.sender, "humPerc", hum);
+      store(sensorData.sender, "presHPa", pres);
+      store(sensorData.sender, "moistPerc", soil);
+    }
   } else {
     console.log("Data received but not signed into Firebase");
   }
 });
+
+const store = function(sensor, quantityId, value) {
+  if(userId != null) {
+    // check if sensor exists in db
+    const sensorId = sensor.toString();
+    const sensorRef = db.collection("sensor").doc(userId).collection("sensor").doc(sensorId);
+    sensorRef.get().then((s) => {
+      // if sensor does not exist, add it
+      if(!s.exists) {
+        sensorRef.set({id: sensorId, name: "sensor" + sensorId })
+        .then((res) => console.log("sensor stored!"))
+        .catch((err) => console.log("sensor store: " + err));
+      }
+    }).catch((err) => console.log("sensor ref: " + err));
+
+    // check if quantity exists
+    const quantityRef = db.collection("sensor").doc(userId).collection("sensor").doc(sensorId).collection("quantity").doc(quantityId);
+    quantityRef.get().then((q) => {
+      if(!q.exists) {
+        quantityRef.set({id: quantityId, name: "quantity" + quantityId, unit: "unit" + quantityId})
+        .then((res) => console.log("quantity stored!"))
+        .catch((err) => console.log("quantity store: " + err));
+      }
+    }).catch((err) => console.log("quantity ref: " + err));
+
+    // store value and time
+    const ref = db.collection("data").doc(userId).collection(sensorId).doc("data").collection(quantityId);
+    const doc = {v: value, t: new Date()};
+    console.log(doc);
+    ref.add(doc).then((result) => {
+      console.log("result: " + result);
+    }).catch((err) => console.log("data store: " + err));
+  } 
+}
 
 app.post('/auth', (req, res) => {
   var credential = firebase.auth.GoogleAuthProvider.credential(req.body);
